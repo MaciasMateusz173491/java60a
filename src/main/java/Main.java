053a5775b23253bb63a52d83cb7637797df2dc75
MaciasMateusz173491 <173491@stud.prz.edu.pl>
2025-05-15 12:46:18 +0000

    zadanie6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,39 +1,28 @@
-/*
-Program sprawdza poprawność wpisywanego imienia. W przypadku wystąpienia spacji w imieniu, funkcja wyrzuca zdefiniowany wyjątek WrongStudentName, który jest wyłapywany w pętli głównej Commit6_0.
-Poniższe zadania będą się sprowadzały do modyfikacji bazowego kodu. Proces modyfikacji ogólnie może wyglądać następująco:
-• Ustalenie jaki błąd chcę się sprawdzić i wyłapać.
-• Decyzja, czy użyje się własnej klasy wyjątku, czy wykorzysta już istniejące (np. Exception, IOException).
-• Napisanie kodu sprawdzającego daną funkcjonalność. W przypadku warunku błędu wyrzucany będzie wyjątek: throw new WrongStudentName().
-• W definicji funkcji, która zawiera kod wyrzucania wyjątku dopisuje się daną nazwę wyjątku, np. public static String ReadName() throws WrongStudentName.
-• We wszystkich funkcjach, które wywołują powyższą funkcję także należy dopisać, że one wyrzucają ten wyjątek – inaczej program się nie skompiluje.
-• W pętli głównej, w main’ie, w zdefiniowanym już try-catch dopisuje się Nazwę wyjątku i go obsługuje, np. wypisuje w konsoli co się stało.
-*/
-
-//Commit6_1. Na podstawie analogii do wyjątku WrongStudentName utwórz i obsłuż wyjątki WrongAge oraz WrongDateOfBirth. 
-//Niepoprawny wiek – gdy jest mniejszy od 0 lub większy niż 100. Niepoprawna data urodzenia – gdy nie jest zapisana w formacie DD-MM-YYYY, np. 28-02-2023.
-
 import java.io.IOException;
 import java.util.Scanner;
 
 class WrongStudentName extends Exception { }
+class WrongAge extends Exception { }
 
 class Main {
     public static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-        while(true) {
+        while (true) {
             try {
                 int ex = menu();
-                switch(ex) {
+                switch (ex) {
                     case 1: exercise1(); break;
                     case 2: exercise2(); break;
                     case 3: exercise3(); break;
                     default: return;
                 }
-            } catch(IOException e) {
-
-            } catch(WrongStudentName e) {
+            } catch (IOException e) {
+                System.out.println("Błąd wejścia-wyjścia!");
+            } catch (WrongStudentName e) {
                 System.out.println("Błędne imię studenta!");
+            } catch (WrongAge e) {
+                System.out.println("Niepoprawny wiek! Wprowadź wiek w przedziale 1-99.");
             }
         }
     }
@@ -47,29 +36,43 @@ class Main {
         return scan.nextInt();
     }
 
-    public static String ReadName() throws WrongStudentName {
+    public static String readName() throws WrongStudentName {
         scan.nextLine();
         System.out.println("Podaj imię: ");
         String name = scan.nextLine();
-        if(name.contains(" "))
+        if (name.contains(" "))
             throw new WrongStudentName();
 
         return name;
     }
 
-    public static void exercise1() throws IOException, WrongStudentName {
-        var name = ReadName();
-        System.out.println("Podaj wiek: ");
-        var age = scan.nextInt();
-        scan.nextLine();
+    public static int readAge() throws WrongAge {
+        int age;
+        while (true) {
+            System.out.println("Podaj wiek (1-99): ");
+            while (!scan.hasNextInt()) {
+                System.out.println("Nieprawidłowy wiek! Wprowadź liczbę całkowitą.");
+                scan.next(); 
+            }
+            age = scan.nextInt();
+            if (age >= 1 && age <= 99) {
+                return age;
+            }
+            System.out.println("Niepoprawny wiek! Wprowadź poprawną wartość.");
+        }
+    }
+
+    public static void exercise1() throws IOException, WrongStudentName, WrongAge {
+        var name = readName();
+        var age = readAge();
         System.out.println("Podaj datę urodzenia DD-MM-YYYY");
-        var date = scan.nextLine();
+        var date = scan.next();
         (new Service()).addStudent(new Student(name, age, date));
     }
 
     public static void exercise2() throws IOException {
         var students = (new Service()).getStudents();
-        for(Student current : students) {
+        for (Student current : students) {
             System.out.println(current.ToString());
         }
     }
@@ -79,7 +82,7 @@ class Main {
         System.out.println("Podaj imię: ");
         var name = scan.nextLine();
         var wanted = (new Service()).findStudentByName(name);
-        if(wanted == null)
+        if (wanted == null)
             System.out.println("Nie znaleziono...");
         else {
             System.out.println("Znaleziono: ");
